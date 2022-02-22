b5957a60ffce74676a9f286f5ff0886d9b7dbadb
Takehito Horiuchi <horiuchi@brains.co.jp>
2022-02-22 14:32:18 +0900

    rust func add

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+mod fib_calcs;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
+use fib_calcs::fib_number::__pyo3_get_function_fibonacci_number;
+use fib_calcs::fib_numbers::__pyo3_get_function_fibonacci_numbers;
+pub mod fib_numbers;
 
 #[pyfunction]
 fn say_hello() {
@@ -9,6 +13,8 @@ fn say_hello() {
 #[pymodule]
 fn flitton_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(say_hello));
+    m.add_wrapped(wrap_pyfunction!(fibonacci_number));
+    m.add_wrapped(wrap_pyfunction!(fibonacci_numbers));
     Ok(())
 }
 
