4a8eb0da20e87d4ca0d63d1601117c615a579c0b
Takehito Horiuchi <horiuchi@brains.co.jp>
2022-03-02 15:37:13 +0900

    add class_module

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,9 @@ use interface::config::__pyo3_get_function_run_config;
 
 use interface::object::__pyo3_get_function_object_interface;
 
+mod class_module;
+use class_module::fib_processor::FibProcessor;
+
 #[pyfunction]
 fn say_hello() {
     println!("saying hello from Rust!");
@@ -24,6 +27,7 @@ fn flitton_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(fibonacci_numbers));
     m.add_wrapped(wrap_pyfunction!(run_config));
     m.add_wrapped(wrap_pyfunction!(object_interface));
+    m.add_class::<FibProcessor>()?;
     Ok(())
 }
 
