eac4d7394527224cba1ea669e7f04f997c383948
Takehito Horiuchi <horiuchi@brains.co.jp>
2022-03-02 19:21:48 +0900

    add vector

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,21 @@ fn say_hello() {
     println!("saying hello from Rust!");
 }
 
+#[pyfunction]
+fn time_add_vectors(total_vector_size: i32) -> Vec<i32> {
+    let mut buffer: Vec<i32> = Vec::new();
+    let first_vector: Vec<i32> =
+        (0..total_vector_size.clone()
+        ).map(|x| x).collect();
+    let second_vector: Vec<i32> =
+        (0..total_vector_size ).map(|x| x).collect();
+    for i in &first_vector {
+        buffer.push(first_vector[**&i as usize] +
+                    second_vector[*i as usize]);
+    }
+    return buffer
+}
+
 #[pymodule]
 fn flitton_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(say_hello));
@@ -27,6 +42,7 @@ fn flitton_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(fibonacci_numbers));
     m.add_wrapped(wrap_pyfunction!(run_config));
     m.add_wrapped(wrap_pyfunction!(object_interface));
+    m.add_wrapped(wrap_pyfunction!(time_add_vectors));
     m.add_class::<FibProcessor>()?;
     Ok(())
 }
